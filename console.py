61c1cd16d64eebf283d41bd764a2316dadf79fcc
odelolajosh <oladapoodelola@gmail.com>
2022-09-14 17:35:34 +0100

    fix: save new instance to storage onCreate

--- console.py
+++ console.py
@@ -161,8 +161,8 @@ class HBNBCommand(cmd.Cmd):
 
         new_instance = HBNBCommand.classes[_cls]()
         new_instance.__dict__.update(kwargs)  # update with params
+        new_instance.save()
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
