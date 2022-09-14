cfc19a2cce749512a6992c60aa2e25edf29c1e1c
Veralee225 <Veralee225@gmail.com>
2022-09-14 23:13:02 +0100

    chore: code update

--- models/place.py
+++ models/place.py
@@ -9,7 +9,7 @@ from os import getenv
 place_amenity = Table('place_amenity', Base.metadata,
                       Column("place_id", String(60), ForeignKey("places.id"),
                              primary_key=True, nullable=False),
-                      Column("amenity_id", ForeignKey("amenities.id"),
+                      Column("amenity_id", String(60), ForeignKey("amenities.id"),
                              primary_key=True, nullable=False)
                       )
 
