7d6148c1f2a992bf08daca4696f3ed7bee3fa948
Veralee225 <Veralee225@gmail.com>
2022-09-14 23:07:39 +0100

    chore: added string to the column

--- models/place.py
+++ models/place.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import relationship
 from os import getenv
 
 place_amenity = Table('place_amenity', Base.metadata,
-                      Column("place_id", ForeignKey("places.id"),
+                      Column("place_id", String(60), ForeignKey("places.id"),
                              primary_key=True, nullable=False),
                       Column("amenity_id", ForeignKey("amenities.id"),
                              primary_key=True, nullable=False)
