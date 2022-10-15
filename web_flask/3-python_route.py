b748b1d7c0dae00a74d51256573a0e91b83256d6
odelolajosh <oladapoodelola@gmail.com>
2022-10-15 16:56:24 +0100

    feat: python prefix route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,30 @@
+""" Hello HBNB """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ hello HBNB route """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def display_hbnb():
+    """ display HBNB route """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_prefix(text):
+    """ C is fun """
+    return "C {}".format(text.replace('_', ' '))
+
+@app.route("/python/<text>", strict_slashes=False)
+def py_prefix(text):
+    """ Python is fun """
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
