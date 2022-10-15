233a48f3679d31022691250a0f62dd365e5395c4
odelolajosh <oladapoodelola@gmail.com>
2022-10-15 17:21:38 +0100

    feat: display a template to show if int variable passed it odd or even

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+""" Hello HBNB """
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ hello HBNB route """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def display_hbnb():
+    """ display HBNB route """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_prefix(text: str):
+    """ C is fun """
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def py_prefix(text: str):
+    """ Python is fun """
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n: int):
+    """ Number only """
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n: int):
+    """ Number only in template """
+    return render_template('5-number.html', number=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n: int):
+    """ Number only in template """
+    return render_template('6-number_odd_or_even.html', number=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
