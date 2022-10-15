030aefd28e9f499ceddbbb2a3c1046446e3f339d
odelolajosh <oladapoodelola@gmail.com>
2022-10-15 21:56:36 +0100

    feat: dissplay all states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" HBNB """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """ Tears down session """
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def display_states():
+    """ Render all the states in storage """
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
