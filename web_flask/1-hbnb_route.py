5a9006c6cf80e672d192004ac21f67139bfc99ba
odelolajosh <oladapoodelola@gmail.com>
2022-10-15 16:55:55 +0100

    feat: hello hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,18 @@
+""" Hello HBNB """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ hello HBNB route """
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def display_hbnb():
+    """ display HBNB route """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
