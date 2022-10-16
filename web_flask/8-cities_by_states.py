b9309442db5a9939a59b14d5b6dcb4fb67b8b4c2
odelolajosh <oladapoodelola@gmail.com>
2022-10-16 21:03:55 +0100

    feat: display all states and cities per state

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" HBNB States with flask """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """ Tears down session """
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def display_states():
+    """ Render all the states in storage """
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def display_cities_by_states():
+    """ Render all cities by state """
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
